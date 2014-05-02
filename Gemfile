90d59447eb937dff93071499d3170d3690cb68e6
Mike Bethany <mike@mikebethany.com>
2014-05-01 20:10:55 -0400

    upgrade ruby

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
-ruby "2.0.0"
-gem 'sinatra', '1.1.0'
+ruby "2.1.1"
+gem 'sinatra', '1.4.5'
+gem 'slim'
+gem 'sass'
+gem 'coffee-script'
--- web.rb
+++ web.rb
@@ -1,5 +1,13 @@
+require 'rubygems'
 require 'sinatra'
+require 'slim'
+require 'sass'
+require 'coffee-script'
 
 get '/' do
-  "Twacker OAuth"
+  slim :index
+end
+
+get '/oauth' do
+  slim :oauth
 end
